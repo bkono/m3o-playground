d8bf3d6e3f80dd933ce718346be0a517ae67adff
Bryan Konowitz <bryan@kono.sh>
2022-02-26 13:17:25 -0800

    feat: added an attempt to use helloworld service

---
+++ go.mod
@@ -0,0 +1,7 @@
+module github.com/bkono/m3o-playground
+
+go 1.17
+
+require go.m3o.com v0.8.0
+
+require github.com/gorilla/websocket v1.4.2 // indirect
--- main.go
+++ main.go
@@ -4,6 +4,9 @@ import (
 	"encoding/json"
 	"io/ioutil"
 	"net/http"
+	"os"
+
+	"go.m3o.com/helloworld"
 )
 
 type Request struct {
@@ -41,9 +44,13 @@ func main() {
 				return
 			}
 
-			if len(req.Name) > 0 {
-				message = "Hello " + req.Name + "!"
-			}
+			// fmt.Println(rsp, err)
+			// if len(req.Name) > 0 {
+
+			// 	message = "Hello " + req.Name + "!"
+			// }
+
+			message = call(req.Name)
 
 			if err := json.NewEncoder(w).Encode(Response{Message: message}); err != nil {
 				w.WriteHeader(500)
@@ -56,12 +63,27 @@ func main() {
 		r.ParseForm()
 		name := r.Form.Get("name")
 
-		if len(name) > 0 {
-			message = "Hello " + name + "!"
+		if len(name) <= 0 {
+			name = "Default"
 		}
+
+		message = call(name)
 
 		w.Write([]byte(message))
 	})
 
 	http.ListenAndServe(":8080", nil)
+}
+
+func call(name string) string {
+	helloworldService := helloworld.NewHelloworldService(os.Getenv("M3O_API_TOKEN"))
+	rsp, err := helloworldService.Call(&helloworld.CallRequest{
+		Name: name,
+	})
+
+	if err != nil {
+		return err.Error()
+	}
+
+	return rsp.Message
 }
